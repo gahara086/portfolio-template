a387ad7920327d8e483b0c145ef52de00cf723cf
gahara <moririn086@gmail.com>
2024-07-23 21:35:41 +0900

    fix build error

--- src/pages/weather.tsx
+++
@@ -1,9 +0,0 @@
-// pages/todo.tsx
-import WeatherApp from '../components/WeatherApp';
-import React from 'react';
-
-const TodoPage: React.FC = () => {
-  return <WeatherApp />;
-}
-
-export default TodoPage;
